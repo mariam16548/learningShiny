64f250c34a90e11cf8b4119ca270485c12531efe
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-07-22 11:40:03 -0700

    Update simpleHelloApp.py

--- simpleFlaskApp/simpleHelloApp.py
+++ simpleFlaskApp/simpleHelloApp.py
@@ -2,18 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/") #remove this block if no html is used
+@app.route("/", methods=["GET", "POST"]) 
 def index(): 
-    return render_template("index.html")
-    
-
-@app.route("/hello", methods=["POST"])
-def hello(): 
-	name= request.form.get("name")
-	return render_template("hello.html",name=name)
-
-
+	note = request.form.get("note")
+	if note is None: 
+		return render_template("hello.html")			
+	else:
+		return render_template("index.html", note=note)
+		
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
-
-    
