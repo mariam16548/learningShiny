08ad9f470f1bce0f04cd7e092aa4054c0d11a7c6
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-07-24 16:39:55 -0700

    Update helloWorld.R

--- simpleHelloApp/helloWorld.R
+++ simpleHelloApp/helloWorld.R
@@ -1,24 +1,42 @@
 library(shiny)
-
 shinyApp( 
-  ui <- fluidPage( 
-    fluidRow( 
+  ui <- fluidPage(
+    tags$head(
+      tags$style("*{font-family: BentonSans Book;}")
+    ),
+    titlePanel(
+      h1("Hello World!",
+         style = "font-family: Times New Roman, font-weight: 500; color: #00b359;")),
+
+    textInput("name", 
+                     label=h5("Please Enter Your Name"), placeholder="Your Name"),
+        tags$style(HTML("
+            .btn {
+              color: #2ecc71;
+              border: 2px #2ecc71 solid;
+            }
+            .btn:hover {
+                color: #fff;
+                background-color: #2ecc71;
+            }
+            .btn-default.active, .btn-default:active, .open > .dropdown-toggle.btn-default {
+                color: #fff;
+                background-color: #2ecc71;
+                border-color: #2ecc71;
+            }
+        ")),
+        actionButton("button", "Submit"),
       column( 
-        3, textInput("name", 
-                     label="Please Enter Your Name"), 
-        actionButton("button", "Submit")), 
-      
-      column( 
-        9, textOutput("value")))
-  ), 
-  
+        9, h3(textOutput("value"), style = "color: #00b359;"))),
+
   server <- function(input, output) {
     
     data <- eventReactive(input$button, {
-      paste("Hello", input$name)
+      paste0("Hello, ", input$name, "!")
     })
     
     output$value <- renderText({
       data()
     })
   })
+shinyApp(ui, server)
