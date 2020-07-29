eb1582880ad456f8d8e3c22c2ce2709e61b19835
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-07-28 17:10:10 -0700

    Update helloWorld.R

--- simpleHelloApp/helloWorld.R
+++ simpleHelloApp/helloWorld.R
@@ -1,41 +1,79 @@
 library(shiny)
-shinyApp( 
-  ui <- fluidPage(
+ui <- fluidPage(
+  fluidRow(
     tags$head(
-      tags$style("*{font-family: BentonSans Book;}")
-    ),
-    titlePanel(
-      h1("Hello World!",
-         style = "font-family: Times New Roman, font-weight: 500; color: #00b359;")),
-
-    textInput("name", 
-                     label=h5("Please Enter Your Name"), placeholder="Your Name"),
-        tags$style(HTML("
+      
+      tags$style(HTML("
+      
+      h1 {
+           display: flex;
+           justify-content: center;
+           align-items: center;
+           height: 100px;
+           width:100%
+           margin-left: 150%;
+       margin-right:0%;
+       
+          }
+                    "))) ,
+    headerPanel( 
+      h1("Hello World!", align = "center",
+         style = "color: #00b359;"))),
+  
+  fluidRow(
+    tags$style(HTML("
+    .form-group {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: auto;
+    }")),
+    textInput("name",  
+              label=h5("Enter your name:"), value="",width="50%",placeholder="Your Name") 
+  ),
+  
+  fluidRow(
+    tags$style(HTML("
             .btn {
               color: #2ecc71;
               border: 2px #2ecc71 solid;
+              margin:auto;
+              display:block;
             }
             .btn:hover {
                 color: #fff;
                 background-color: #2ecc71;
+                margin:auto;
+                display:block;
             }
             .btn-default.active, .btn-default:active, .open > .dropdown-toggle.btn-default {
                 color: #fff;
                 background-color: #2ecc71;
                 border-color: #2ecc71;
-            }
+                margin:auto;
+                display:block; }         
         ")),
-        actionButton("button", "Submit"),
-      column( 
-        9, h3(textOutput("value"), style = "color: #00b359;"))),
+    actionButton("button", "Submit")),
+  
+  fluidRow(
+    tags$style(HTML("
+    h2 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin:auto
+    }")),
+    h2(textOutput("value"),
+       style = "color: #66cc66;")))
 
-  server <- function(input, output) {
-    
-    data <- eventReactive(input$button, {
-      paste0("Hello, ", input$name, "!")
-    })
-    
-    output$value <- renderText({
-      data()
-    })
+server <- function(input, output) {
+  
+  data <- eventReactive(input$button, {
+    paste0("Hello,"," ", input$name, "!")
   })
+  
+  output$value <- renderText({
+    data()
+  })
+}
+shinyApp(ui=ui, server=server)
