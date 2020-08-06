999cdf11bbad412a20d39bbdbcabe789c745ae18
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-08-06 12:22:16 -0700

    removed the evaluate button

--- covidProject/covidApp/covidSimpleCalculation.R
+++ covidProject/covidApp/covidSimpleCalculation.R
@@ -9,7 +9,7 @@ shinyApp(
     div(
       id = "form",
       sliderInput("popDensity", "What is the population density in your county (in people/square mile)?", value=0, min=0, max=1200),
-
+      
       
       tags$style(HTML("
             .btn {
@@ -19,39 +19,37 @@ shinyApp(
               display:block;
             }
         ")),
-      actionButton("button", "Evaluate", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
       uiOutput("calculation")
-
     )),
- 
+  
+  
   server = function(input, output) {
+    
+      likelihoodOfHarm<-reactive({(1/500)*input$popDensity})
 
-      observeEvent(input$button, {
-      likelihoodOfHarm<-(1/500)*input$popDensity
-        
+      
       output$calculation<-renderUI({
-      if (likelihoodOfHarm>1) {
-        sidebarPanel(style="background-color: red; width: 300px; height: 300px;", h3("Extreme risk, stay home!"))
-        
-        } else if (likelihoodOfHarm>.65){
+        if (likelihoodOfHarm()>1) {
+          sidebarPanel(style="background-color: red; width: 300px; height: 300px;", h3("Extreme risk, stay home!"))
+          
+        } else if (likelihoodOfHarm()>.65){
           sidebarPanel(style="background-color: orange; width: 300px; height: 300px;", 
                        h3("Very high risk, stay home!"))
         }
-        else if (likelihoodOfHarm>.35){
+        else if (likelihoodOfHarm()>.35){
           sidebarPanel(style="background-color: yellow; width: 300px; height: 300px;", 
                        h3("High risk, be careful!"))
         }
-        else if (likelihoodOfHarm>.10){
+        else if (likelihoodOfHarm()>.10){
           sidebarPanel(style="background-color: blue; width: 300px; height: 300px;", 
                        h3("Moderate risk, be careful!"))
           
         } else {
-         sidebarPanel(style="background-color: #39ac39; width: 300px; height: 300px;", 
-                      h3("Low risk, but still be careful!"))
-       }
-
-    })
-      }) 
-           
+          sidebarPanel(style="background-color: #39ac39; width: 300px; height: 300px;", 
+                       h3("Low risk, but still be careful!"))
+          
+        }
+        
+      })
   }
 )
