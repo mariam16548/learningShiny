34dab24ff3da5a2e2e68b53cee00f57578cb64a1
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-08-06 17:33:42 -0700

    Update covidSimpleCalculation.R

--- covidProject/covidApp/covidSimpleCalculation.R
+++ covidProject/covidApp/covidSimpleCalculation.R
@@ -1,47 +1,51 @@
 library(shiny)
 library(shinyWidgets)
 
+displayColoredBox<- function(color, riskMessage){
+  sidebarPanel(style=sprintf("background-color: %s; width: 300px; height: 300px;", color),
+               h3(sprintf("%s", riskMessage)) )  }
 shinyApp(
   ui = fluidPage(
-
+    
     titlePanel("Will your actions today propagate the virus to the community?"),
-
+    
     div(
       id = "form",
-      sliderInput("popDensity", "What is the population density in your county (in people/square mile)?", value=0, min=0, max=1200),
+      sliderInput("populationDensity", "What is the population density in your county (in people/square mile)?", value=0, min=0, max=1200),
       
-      uiOutput("calculation")
+      uiOutput("coloredBox")
     )),
-
-
+  
   server = function(input, output) {
+    
+    output$coloredBox<-renderUI({
+      req(input$populationDensity)
+      populationDensity <- input$populationDensity;
+      likelihoodOfHarm <- populationDensity/500
+      
+      
+      if (likelihoodOfHarm>1) {
+        color="red"
+        riskMessage="Extreme risk, stay home!"
 
-      output$calculation<-renderUI({
-          req(input$popDensity)
-          populationDensity <- input$popDensity;
-          likelihoodOfHarm <- populationDensity/500
-
-        if (likelihoodOfHarm>1) {
-          sidebarPanel(style="background-color: red; width: 300px; height: 300px;", h3("Extreme risk, stay home!"))
-
-        } else if (likelihoodOfHarm>.65){
-          sidebarPanel(style="background-color: orange; width: 300px; height: 300px;",
-                       h3("Very high risk, stay home!"))
-        }
-        else if (likelihoodOfHarm>.35){
-          sidebarPanel(style="background-color: yellow; width: 300px; height: 300px;",
-                       h3("High risk, be careful!"))
-        }
-        else if (likelihoodOfHarm>.10){
-          sidebarPanel(style="background-color: blue; width: 300px; height: 300px;",
-                       h3("Moderate risk, be careful!"))
-
-        } else {
-          sidebarPanel(style="background-color: #39ac39; width: 300px; height: 300px;",
-                       h3("Low risk, but still be careful!"))
-
-        }
-
-      })
+      } else if (likelihoodOfHarm>.65){
+        color="orange"
+        riskMessage="Very high risk, stay home!"
+      }
+      else if (likelihoodOfHarm>.35){
+        color="yellow"
+        riskMessage="High risk, be careful!"
+      }
+      else if (likelihoodOfHarm>.10){
+        color="blue"
+        riskMessage="Moderate risk, be careful!"
+      } else {
+        color="green"
+        riskMessage="Low risk, but still be careful!"
+      }
+      
+      displayColoredBox(color, riskMessage)
+      
+    })
   }
 )
