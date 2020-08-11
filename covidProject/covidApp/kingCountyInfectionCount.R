c099578a6161d8b25d8eeafb7ccd9e88003c9127
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-08-11 14:33:39 -0700

    Update kingCountyInfectionCount.R

--- covidProject/covidApp/kingCountyInfectionCount.R
+++ covidProject/covidApp/kingCountyInfectionCount.R
@@ -4,6 +4,6 @@ url <- "https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJ
 data=content(GET(url), type = 'text/csv')
 
 data1<-subset(data, Type!="Deaths") #only has cases stats not death stats
-data2 %>% map_df(rev) #flip row order so that stats start from earliest date and ends with latest date
+data2=data1 %>% map_df(rev) #flip row order so that stats start from earliest date and ends with latest date
 
 barplot(data2$"7_day_count", main="Weekly COVID-19 Case Count in King County",xlab="Weeks since 1/29/20", ylab="Weekly Infection Count")
