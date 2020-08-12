da6c70dd49da8187c287d1d65d8fbcd17686e896
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-08-12 10:32:27 -0700

    Update kingCountyInfectionCount.R

--- covidProject/covidApp/kingCountyInfectionCount.R
+++ covidProject/covidApp/kingCountyInfectionCount.R
@@ -1,9 +1,9 @@
 library(httr) 
-library(tidyverse)
 url <- "https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=County%207%20day%20growth%20rates&exportFormat=csv&c9SqlFilter=select%20*%20where%20County%20like%20King%20County%20AND%20State%20like%20Washington"
-data=content(GET(url), type = 'text/csv')
+tbl.kingCounty <- content(GET(url), type = 'text/csv')
 
-data1<-subset(data, Type!="Deaths") #only has cases stats not death stats
-data2=data1 %>% map_df(rev) #flip row order so that stats start from earliest date and ends with latest date
+tbl.cases <- subset(tbl.kingCounty, Type!="Deaths") #only has cases not death stats
+tbl.cases <- tbl.cases[seq(dim(tbl.cases)[1],1),] #flip row order so that stats start from earliest date and ends with latest date
 
-barplot(data2$"7_day_count", main="Weekly COVID-19 Case Count in King County",xlab="Weeks since 1/29/20", ylab="Weekly Infection Count")
+barplot(tbl.cases$"7_day_count", main="Weekly COVID-19 Case Count in King County", 
+        xlab="Weeks since 1/29/20", ylab="Weekly Infection Count")
