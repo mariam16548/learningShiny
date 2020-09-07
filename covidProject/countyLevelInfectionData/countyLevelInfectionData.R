e5264009d764869a165924ad3867559301f7f9ea
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-09-07 01:41:59 -0700

    added in an error message for invalid zipcodes

--- covidProject/countyLevelInfectionData/countyLevelInfectionData.R
+++ covidProject/countyLevelInfectionData/countyLevelInfectionData.R
@@ -1,5 +1,5 @@
 source("zipcodeToCounty.R")
-countyLevelInfectionData<- function(zip, startDate=NA, endDate=NA) { #dates must be written in "XX/XX/XXXX" format
+countyLevelInfectionData<- function(zip, startDate=NA, endDate=NA) { #dates must be written in "XX/XX/XXXX" format (make sure written as a STRING)
   countyStateNames <- zipcodeToCounty(zip)
   countyName <- countyStateNames[1]
   stateName <- countyStateNames[2]
@@ -11,30 +11,34 @@ countyLevelInfectionData<- function(zip, startDate=NA, endDate=NA) { #dates must
                                 "%20County%20AND%20State%20like%20", 
                                 gsub(" " , "%20", stateName))
   tbl.countyStats <- content(GET(urlCountyInfectionStats), type = 'text/csv')
-  tbl.countyCases <- subset(tbl.countyStats, Type!="Deaths") #only has cases not death stats
-  tbl.countyCases <- tbl.countyCases[seq(dim(tbl.countyCases)[1],1),] #flip row order 
-  tbl.countyCases <- subset(tbl.countyCases, select = c('7_day','7_day_count')) #only has date and case stats
-  names(tbl.countyCases)[1] <- "weekDate"
-  names(tbl.countyCases)[2] <- "caseCount"
-  if (is.na(startDate)) {  #check if the startDate or endDates are NA-- defaults to giving earliest/latest data, respectively 
-    startDate <- tbl.countyCases$weekDate[1]
-  } else {
-    startDate <- startDate
-  }
-  
-  if (is.na(endDate)) {
-    endDate<- tail(tbl.countyCases$weekDate,1)
-  } else {
-    endDate <- endDate
-  }
-  
-  startDateRowNumber <- which(grepl(startDate, tbl.countyCases$weekDate)) #find row number of the startDate
-  endDateRowNumber <- which(grepl(endDate, tbl.countyCases$weekDate)) #find row number of the endDate
-  if (length(startDateRowNumber)==0 || length(endDateRowNumber)==0) {
-    print("The dates you selected are not within the available range, please select a start date that is 01/22/2020 or later.")
+  if (length(tbl.countyStats)==0) {
+    print("The selected zipcode is invalid. Please try again.")
   }  else {
-    startDateRowNumber <- startDateRowNumber
-    tbl.countyCases <- tbl.countyCases[startDateRowNumber:endDateRowNumber,,drop=F]
-    print(data.frame(tbl.countyCases), row.names = FALSE)
-  }
+      tbl.countyCases <- subset(tbl.countyStats, Type!="Deaths") #only has cases not death stats
+      tbl.countyCases <- tbl.countyCases[seq(dim(tbl.countyCases)[1],1),] #flip row order 
+      tbl.countyCases <- subset(tbl.countyCases, select = c('7_day','7_day_count')) #only has date and case stats
+      names(tbl.countyCases)[1] <- "weekDate"
+      names(tbl.countyCases)[2] <- "caseCount"
+      if (is.na(startDate)) {  #check if the startDate or endDates are NA-- defaults to giving earliest/latest data, respectively 
+        startDate <- tbl.countyCases$weekDate[1]
+      } else {
+        startDate <- startDate
+      }
+      
+      if (is.na(endDate)) {
+        endDate<- tail(tbl.countyCases$weekDate,1)
+      } else {
+        endDate <- endDate
+      }
+      
+      startDateRowNumber <- which(grepl(startDate, tbl.countyCases$weekDate)) #find row number of the startDate
+      endDateRowNumber <- which(grepl(endDate, tbl.countyCases$weekDate)) #find row number of the endDate
+      if (length(startDateRowNumber)==0 || length(endDateRowNumber)==0) {
+        print("The dates you selected are not within the available range, please select a start date that is 01/22/2020 or later.")
+      }  else {
+        startDateRowNumber <- startDateRowNumber
+        tbl.countyCases <- tbl.countyCases[startDateRowNumber:endDateRowNumber,,drop=F]
+        print(data.frame(tbl.countyCases), row.names = FALSE)
+      }
+            }
 }
