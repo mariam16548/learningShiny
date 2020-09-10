d7f402cfcafc9573e755f0ca3426b27b2e96f505
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-09-10 12:20:57 -0700

    added a comment to show data source

--- covidProject/covidAppV2/countyPopulationDensity.R
+++ covidProject/covidAppV2/countyPopulationDensity.R
@@ -7,3 +7,5 @@ countyPopulationDensity<- function(countyName, stateName) {
   tbl.county[3]<- tbl.county[3]*2.58998811 #convert from people/sq.km to people/sq.miles
   tbl.county$`County Population Density` #returns the county population density 
 }
+#source for data (from Census): 
+#https://covid19.census.gov/datasets/21843f238cbb46b08615fc53e19e0daf/data?geometry=-126.247%2C28.795%2C126.878%2C67.148&selectedAttribute=B01001_calc_PopDensity
