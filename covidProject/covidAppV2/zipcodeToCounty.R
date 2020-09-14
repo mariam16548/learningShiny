7668c110100f13c05492f07d745cedb4e318665c
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-09-14 10:27:35 -0700

    load the data before the function is called

--- covidProject/covidAppV2/zipcodeToCounty.R
+++ covidProject/covidAppV2/zipcodeToCounty.R
@@ -1,7 +1,8 @@
 library(httr) 
+urlCounties <- "https://raw.githubusercontent.com/scpike/us-state-county-zip/master/geo-data.csv"
+zipcodeToCountyData <- content(GET(urlCounties), type = 'text/csv')
+
 zipcodeToCounty<- function(zip) {
-  urlCounties <- "https://raw.githubusercontent.com/scpike/us-state-county-zip/master/geo-data.csv"
-  zipcodeToCountyData <- content(GET(urlCounties), type = 'text/csv')
   tbl.county <- subset(zipcodeToCountyData, zipcode == zip) 
   countyName <- tbl.county$county
   stateName <- tbl.county$state
