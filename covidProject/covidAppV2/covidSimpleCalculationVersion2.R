39ad00db15e25fc6b46a99a8cdc4312f2808a9d9
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-09-15 14:02:21 -0700

    simplied code by making risk calculation separate

--- covidProject/covidAppV2/covidSimpleCalculationVersion2.R
+++ covidProject/covidAppV2/covidSimpleCalculationVersion2.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(RUnit)
 
-source("countyLevelInfectionData.R")
+source("riskCalculation.R")
 
 displayColoredBox<- function(color, riskMessage){
   sidebarPanel(style=sprintf("background-color: %s; width: 550px; height: 120px;", color),
@@ -15,7 +15,7 @@ app <- shinyApp(
                   sidebarPanel("",
                                textInput("zipcode", label="Enter your zipcode.", value = 98125), 
                                numericInput("age", label="Enter your age.", value = ""),
-                               numericInput("groupSize", label="How many people will be with you?", value = ""),
+                               numericInput("groupSize", label="How many people (outside your household) will be with you?", value = ""),
                                radioButtons("masking", "Will you wear a mask?",
                                            c("Yes", "No")),
                                radioButtons("alcoholConsumption", "Will you be under the influence of alcohol outside of your home?",
@@ -32,7 +32,7 @@ app <- shinyApp(
     observeEvent(input$zipcode,{     #limits zipcode input to 5 numbers only
       if(nchar(input$zipcode)!=5)
       {
-        updateTextInput(session,'zipcode',value=66101)
+        updateTextInput(session,'zipcode',value=98125)
         showModal(modalDialog(
           title = "Error!",
           "Only 5-character entries are permitted.",
@@ -49,7 +49,6 @@ app <- shinyApp(
       }
     }
     )
-    
     
     getInfectionData <- reactive({
       req(input$zipcode)
@@ -73,57 +72,22 @@ app <- shinyApp(
       groupSize <- input$groupSize
       alcoholConsumption <- input$alcoholConsumption
       
-      
-      infectionData <- data.frame(countyLevelInfectionData(zipcode))
-      countyPopulatonDensity<-infectionData[1, 3] #extract the county population density
-      
-      mostRecentCaseCount<- tail(infectionData$caseCount,1)
-      likelihoodOfHarm <- (countyPopulatonDensity*mostRecentCaseCount)/100000000
-      
-      if (masking=="Yes") {                    #decreased risk because more precautions are being taken
-        likelihoodOfHarm<- likelihoodOfHarm*.35
-      }  else {
-        likelihoodOfHarm <- likelihoodOfHarm
-        }
+      likelihoodOfHarm<- riskCalculation(zipcode, masking, age, groupSize, alcoholConsumption)
 
-      if (age<30){
-        likelihoodOfHarm <- likelihoodOfHarm*1.31     #increased risk because more likely to exhibit risky behavior
-      }  else {
-        likelihoodOfHarm <- likelihoodOfHarm
-      }
-      
-      if (groupSize>10){
-        likelihoodOfHarm <- likelihoodOfHarm*1.3 #increased risk because people around in close proximity
-      } else if (groupSize>25){
-         likelihoodOfHarm <- likelihoodOfHarm*1.6 #increased risk because more people around in close proximity
-      } else if (groupSize>50){
-        likelihoodOfHarm <- likelihoodOfHarm*1.9 #increased risk because more people around in close proximity
-      }  else {
-        likelihoodOfHarm <- likelihoodOfHarm
-      }
-      
-      if (alcoholConsumption=="Yes"){
-        likelihoodOfHarm <- likelihoodOfHarm*1.66 #increased risk because lack of awareness
-      }  else {
-        likelihoodOfHarm <- likelihoodOfHarm
-      }
-      
-      
-      
   #designing the coloredBox
       if (likelihoodOfHarm>1) {
         color<-"red"
         riskMessage<-"Extreme risk, stay home!"
         
-      } else if (likelihoodOfHarm>.65){
+      } else if (likelihoodOfHarm>.75){
         color<-"orange"
         riskMessage<-"Very high risk, stay home!"
       }
-      else if (likelihoodOfHarm>.35){
+      else if (likelihoodOfHarm>.45){
         color<-"yellow"
         riskMessage<-"High risk, be careful!"
       }
-      else if (likelihoodOfHarm>.10){
+      else if (likelihoodOfHarm>.20){
         color<-"#4d94ff" #this shade of blue isn't too dark
         riskMessage<-"Moderate risk, be careful!"
       } else {
@@ -138,7 +102,6 @@ app <- shinyApp(
       riskAndColor<-getRiskAndColor() #the list/result of the function goes into a variable called riskAndColor
       displayColoredBox(riskAndColor$color, riskAndColor$riskMessage) #extract certain elements from the list to plug into the displayColoredBox() function
       #the output will be the output of the displayColorBox() function
-      
     })
     
     output$histogramOfCases <- renderPlot({
@@ -146,7 +109,6 @@ app <- shinyApp(
       plot(1:nrow(infectionData), infectionData$caseCount, main="COVID-19 Case Count in Your Area", 
            xlab="Days since 01/22/2020", ylab="Infection Count", cex.main=1.5, cex.lab=1.3, cex.axis=1.2, col="red", "h")
     })
-    
   }
 )
 
@@ -158,24 +120,10 @@ testServer(app, {
   session$setInputs(alcoholConsumption = "No")  #set input as no alcohol consumption
   
   riskAndColor<-getRiskAndColor() #the list/result of the function goes into a variable called riskAndColor
-  checkTrue(riskAndColor$likelihoodOfHarm< 0.65)  #extract certain element from the list to check its value
+  checkEquals(riskAndColor$likelihoodOfHarm, 0.09616751, tolerance = 10^-7)  #extract certain element from the list to check its value
   cat("Correct likelihood of harm value!\n") #if previous line is true, paste the given message 
   checkEquals(riskAndColor$riskMessage, "Low risk, but still be careful!") #extract certain element from the list to check its value
   cat("Correct risk message!\n") #if previous line is true, paste the given message 
   checkEquals(riskAndColor$color, "#bfff80") #extract certain element from the list to check its value
   cat("Correct color!\n") #if previous line is true, paste the given message 
 })
-#source for mask lowering risk calculation: 
-#https://www.ucdavis.edu/coronavirus/news/your-mask-cuts-own-risk-65-percent/
-
-#source for alcohol causing lowered awareness and increasing risk calculation: --- .08 BAC results in avg of 66% impairment
-#https://one.nhtsa.gov/people/injury/research/pub/HS809028/index_.htm
-
-#source for younger people more likely to exhibit risky behavior: 
-  #https://komonews.com/news/coronavirus/troubling-trend-shows-more-young-people-testing-positive-for-covid-19
-  #https://www.usatoday.com/story/news/politics/2020/06/26/coronavirus-young-americans-less-likely-social-distance/3257513001/
-    # 45% of people under 30 are socializing with people they do not live with and not social distancing
-    # Around 40% of COVID cases are from people under 30
-      # Therefore, around 16% of young people (likely asymptomatic) are engaging in risky behavior (.40*.45)
-    # 82% risk reduction when social distancing (https://newsroom.unsw.edu.au/news/health/covid-19-how-much-do-social-distancing-and-masks-reduce-risk)
-    #Calculation of risk increase: 1-(.82*(1-.16))=.31 -> 31% risk increase for people under age of 30 to spread the infection
